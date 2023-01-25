1743b6a1d365e1536ac3d421d74cbe03ba7927b8
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-01-25 17:05:14 +0200

    Add test for reverseString function

--- function.test.js
+++ function.test.js
@@ -3,13 +3,37 @@ const stringLength = (string) => {
 };
 
 it('Returns the length of the string Micronaut', () => {
-  expect(stringLength('Micronaut')).toBe(9);
+  const str = 'Micronaut';
+
+  const length = stringLength(str);
+
+  expect(length).toBe(9);
 });
 
 it('Returns an error message if string is greater than 10', () => {
-  expect(stringLength('Micronautist')).toBe('error');
+  const str = 'Micronautist';
+
+  const length = stringLength(str);
+
+  expect(length).toBe('error');
 });
 
 it('Returns an error message if string is less than 1', () => {
-  expect(stringLength('')).toBe('error');
+  const str = '';
+
+  const length = stringLength(str);
+
+  expect(length).toBe('error');
+});
+
+const reverseString = (string) => {
+  return string.trim().split('').reverse().join('');
+};
+
+it('Returns a revesed string that is not equal to the original', () => {
+  const str = 'Awesome';
+
+  const reversedStr = reverseString(str);
+
+  expect(reversedStr).not.toBe(str);
 });
